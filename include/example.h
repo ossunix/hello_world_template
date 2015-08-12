2fbdc0d7f97c069dc1588d18074b7b8a9c758cdb
Aksel Meola <aksel@meola.eu>
2015-08-12 12:01:56 +0300

    Created the template.

---
+++ include/example.h
@@ -0,0 +1,9 @@
+/**
+ *	Example.H
+ *
+ */
+
+/**
+ *	@param text - Text to print on the screen.
+ */
+void example_printf(char *text);
---
+++ src/example.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <glib.h>
+
+/*	Print text on the screen */
+void example_printf(char *text)
+{
+	printf("%s", text);
+	return;
+}
+
